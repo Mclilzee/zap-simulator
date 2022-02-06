2125c7ca4b42b697e4625d542cf5601b3366b753
DigitalKnight <asadredset@gmail.com>
2022-02-07 02:05:29 +0500

    Added offense feature

--- index.js
+++ index.js
@@ -1,83 +1,30 @@
-const tierPoints = { 0: 0, 1: 1, 2: 2, 3: 5, 4: 10};
-let tierList = [0, 1, 2, 3, 4];
-let currentZapPoints = 0;
+const form = document.querySelector(".lvl1")
+const button = document.querySelector("#zapIndex1")
 
-const zapButtons = document.querySelector(".zapButtons");
+button.addEventListener("click", () => {
+  form.classList.remove("hidden")
+})
 
-zapButtons.addEventListener("click", (e) => {
-  const index = Number(e.target.id.slice(-1));
+form.addEventListener("click", (e) => {
+  let val = e.target.dataset.point;
+  let color = e.target.classList[0]
+  let cell = get_Cell(val)
+  let block = get_block(color)
+  cell.appendChild(block)
+  e.target.dataset.point = Number(val) + 1;
+  form.classList.add("hidden")
+  
+})
 
-  commitZap(index);
-});
-
-const timeTravelButton = document.querySelector(".timeTravelButton");
-
-timeTravelButton.addEventListener("click", () => {
-  currentZapPoints--;
-
-  if (currentZapPoints < 0) {
-    currentZapPoints = 0;
-  }
-
-  setZapPointsLabel();
-});
-
-const tryAgainButton = document.querySelector(".tryAgainButton");
-
-tryAgainButton.addEventListener("click", () => {
-  document.querySelector(".bannMessage").classList.add("hidden");
-
-  currentZapPoints = 0;
-  setZapPointsLabel();
-  resetBoard();
-});
-
-function resetBoard() {
-  tierList = [0, 1, 2, 3, 4];
-  emptyAllCells();
+const get_Cell = (val) => {
+  let cell = document.querySelector(`#d${val}`)
+  return cell
 }
 
-function emptyAllCells() {
-  document.querySelectorAll(".cell").forEach((cell) => {
-    cell.classList.remove("zapped");
-  });
-}
-
-function commitZap(index) {
-  const zapRank = tierList[index];
-  const zapResult = tierPoints[zapRank];
-  tierList[index] += 1;
-
-  currentZapPoints += zapResult;
-
-  updateBoard(index, zapRank);
-
-  if (currentZapPoints >= 10) {
-    getBanned();
-  }
-}
-
-function updateBoard(index, zapRank) {
-  setZapPointsLabel();
-
-  const cellId = "" + index + zapRank;
-  const cellToUpdate = document.getElementById(cellId);
-  cellToUpdate.classList.add("zapped");
-}
-
-function setZapPointsLabel() {
-  const zapPointsLabel = document.querySelector(".zapPointsLabel");
-  zapPointsLabel.textContent = "Current Zap Points: " + currentZapPoints;
-
-  if (currentZapPoints >= 10) {
-    zapPointsLabel.id = "currentPointsRank3";
-  } else if (currentZapPoints >= 5) {
-    zapPointsLabel.id = "currentPointsRank2";
-  } else {
-    zapPointsLabel.id = "currentPointsRank1";
-  }
-}
-
-function getBanned() {
-  document.querySelector(".bannMessage").classList.remove("hidden");
+const get_block = (color) => {
+  console.log(color)
+  let div = document.createElement("div")
+  div.style.backgroundColor = color
+  div.style.flex = 1;
+  return div
 }
