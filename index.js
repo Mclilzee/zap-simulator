08ad439e27df248f8e3303f779eca932c283f3b6
Mclilzee <mc.lilzee@hotmail.com>
2022-02-18 10:04:05 +0100

    Create disclaimer message screen

--- index.js
+++ index.js
@@ -2,31 +2,38 @@ const tierPoints = { 0: 0, 1: 1, 2: 2, 3: 5, 4: 10 };
 let total_points = 0;
 let form;
 
+const disclaimerConfirmButton = document.querySelector(
+  ".disclaimerConfirmButton",
+);
+disclaimerConfirmButton.addEventListener("click", () => {
+  document.querySelector(".disclaimerScreen").remove();
+});
+
 (function () {
   //adds tier tags
-  const buttons = document.querySelectorAll('.levels button');
+  const buttons = document.querySelectorAll(".levels button");
   buttons.forEach((button) => {
-    const tag = document.createElement('span');
-    tag.classList.add('tier-tag');
+    const tag = document.createElement("span");
+    tag.classList.add("tier-tag");
     button.parentNode.insertBefore(tag, button.nextSibling);
   });
 })();
 
 //if you go with the tier level I suggest the below order for Tier 0 to 10:
 const svg_paths = {
-  0: './images/zaps/TOPzap-shade-1.svg', //tier 0  #ffdc2f
-  1: './images/zaps/TOPzap-shade-2.svg', //tier 1  #eeb434
-  2: './images/zaps/TOPzap-shade-3.svg', //tier 2  #e09034
-  3: './images/zaps/TOPzap-shade-4.svg', //tier 5  #d47032
-  4: './images/zaps/TOPzap-shade-5.svg', //tier 10 #be1e2d
+  0: "./images/zaps/TOPzap-shade-1.svg", //tier 0  #ffdc2f
+  1: "./images/zaps/TOPzap-shade-2.svg", //tier 1  #eeb434
+  2: "./images/zaps/TOPzap-shade-3.svg", //tier 2  #e09034
+  3: "./images/zaps/TOPzap-shade-4.svg", //tier 5  #d47032
+  4: "./images/zaps/TOPzap-shade-5.svg", //tier 10 #be1e2d
 };
 
 const color_codes = {
-  0: '#ffdc2f',
-  1: '#eeb434',
-  2: '#e09034',
-  3: '#d47032',
-  4: '#be1e2d',
+  0: "#ffdc2f",
+  1: "#eeb434",
+  2: "#e09034",
+  3: "#d47032",
+  4: "#be1e2d",
 };
 
 const generate_level = (ind, o_tier, c_tier) => {
@@ -41,7 +48,7 @@ const generate_level = (ind, o_tier, c_tier) => {
 };
 const create_levels = () => {
   return Object.keys(tierPoints).map((tier) =>
-    Object.keys(svg_paths).map((ind) => generate_level(ind, tier, tier))
+    Object.keys(svg_paths).map((ind) => generate_level(ind, tier, tier)),
   );
 };
 
@@ -53,7 +60,7 @@ const get_object = (ind, tier) => {
 
 const update_buttons = (form) => {
   const buttons = [...form.childNodes].filter(
-    (node) => node.tagName == 'BUTTON'
+    (node) => node.tagName == "BUTTON",
   );
   buttons.forEach((button) => {
     const classes = button.classList;
@@ -63,23 +70,23 @@ const update_buttons = (form) => {
   });
 };
 
-const zapButtons = document.querySelectorAll('.zapButton');
+const zapButtons = document.querySelectorAll(".zapButton");
 
 zapButtons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener("click", (e) => {
     const index = Number(e.target.id.slice(-1));
     form = document.querySelector(`.lvl${index}`);
     update_buttons(form);
-    form.classList.remove('hidden');
+    form.classList.remove("hidden");
   });
 });
 
 const get_stats = (obj, offense) => {
-  const points = document.createElement('div');
-  points.style.fontWeight = 'bold';
-  const name = document.createElement('div');
-  const c_points = document.createElement('div');
-  points.classList.add('t_points');
+  const points = document.createElement("div");
+  points.style.fontWeight = "bold";
+  const name = document.createElement("div");
+  const c_points = document.createElement("div");
+  points.classList.add("t_points");
   points.textContent = `Current Zap Points: ${total_points}`;
   name.textContent = `Last offense committed: ${offense}`;
   c_points.textContent = `${offense}'s tier moved: ${
@@ -90,11 +97,11 @@ const get_stats = (obj, offense) => {
 
 const update_stats = (obj, offense) => {
   total_points += tierPoints[obj.c_tier];
-  const display_stats = document.querySelector('.zapPointsLabel');
+  const display_stats = document.querySelector(".zapPointsLabel");
   const stats = get_stats(obj, offense);
-  display_stats.textContent = '';
+  display_stats.textContent = "";
   stats.forEach((stat) => {
-    stat.classList.add('stat');
+    stat.classList.add("stat");
     display_stats.appendChild(stat);
   });
 };
@@ -123,22 +130,22 @@ const set_images = (cell_imgs) => {
 
 const update_chart = (obj) => {
   const cell = document.querySelector(`#t${obj.o_tier}${obj.c_tier}`);
-  const div = document.createElement('div');
-  div.classList.add('img-container');
+  const div = document.createElement("div");
+  div.classList.add("img-container");
   div.style.backgroundImage = `url(${svg_paths[obj.c_tier]})`;
   cell.appendChild(div);
   const cell_imgs = document.querySelectorAll(
-    `#t${obj.o_tier}${obj.c_tier} > div`
+    `#t${obj.o_tier}${obj.c_tier} > div`,
   );
   const set_imgs = set_images(cell_imgs);
-  cell.textContent = '';
+  cell.textContent = "";
   set_imgs.forEach((image) => {
     cell.appendChild(image);
   });
 };
 
 const getBanned = () => {
-  document.querySelector('.bannMessage').classList.remove('hidden');
+  document.querySelector(".bannMessage").classList.remove("hidden");
 };
 
 const check_ban = () => {
@@ -147,11 +154,11 @@ const check_ban = () => {
   }
 };
 
-const forms = document.querySelectorAll('.form');
+const forms = document.querySelectorAll(".form");
 forms.forEach((eachForm) => {
   // eachForm is to avoid name conflict with form
-  eachForm.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
+  eachForm.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
       const classes = e.target.classList;
       const ind = classes[1][1];
       const tier = Number(classes[0].slice(-1));
@@ -161,32 +168,32 @@ forms.forEach((eachForm) => {
       check_ban();
       update_object(obj);
     }
-    form.classList.add('hidden');
+    form.classList.add("hidden");
   });
 });
 
 const resetSimulator = () => {
   levels = create_levels().flat();
   total_points = 0;
-  document.querySelectorAll('.cell').forEach((cell) => {
-    cell.textContent = '';
+  document.querySelectorAll(".cell").forEach((cell) => {
+    cell.textContent = "";
   });
-  document.querySelector('.zapPointsLabel').textContent =
-    'Welcome, you are clean right now';
-  document.querySelector('.bannMessage').classList.add('hidden');
+  document.querySelector(".zapPointsLabel").textContent =
+    "Welcome, you are clean right now";
+  document.querySelector(".bannMessage").classList.add("hidden");
 };
 
-const tryAgainButton = document.querySelector('.tryAgainButton');
-tryAgainButton.addEventListener('click', resetSimulator);
+const tryAgainButton = document.querySelector(".tryAgainButton");
+tryAgainButton.addEventListener("click", resetSimulator);
 
-const resetButton = document.querySelector('.resetButton');
-resetButton.addEventListener('click', resetSimulator);
+const resetButton = document.querySelector(".resetButton");
+resetButton.addEventListener("click", resetSimulator);
 
-const timeTravelButton = document.querySelector('.timeTravelButton');
+const timeTravelButton = document.querySelector(".timeTravelButton");
 
-timeTravelButton.addEventListener('click', () => {
+timeTravelButton.addEventListener("click", () => {
   total_points < 1 ? total_points : total_points--;
-  const points = document.querySelector('.t_points');
+  const points = document.querySelector(".t_points");
   points ? (points.textContent = `Current Zap Points: ${total_points}`) : null;
 });
 
@@ -198,28 +205,28 @@ const verifyForMoon = (e) => {
   return e.target.classList.contains("moonSvg");
 };
 
-const darkModeButton = document.querySelector('.dmb');
+const darkModeButton = document.querySelector(".dmb");
 
 const swap = (e) => {
   if (verifyForMoon(e)) {
-    darkModeButton.textContent = '';
-    console.log('yay');
-    const sun = document.querySelector('.sun').content.cloneNode(true);
+    darkModeButton.textContent = "";
+    console.log("yay");
+    const sun = document.querySelector(".sun").content.cloneNode(true);
     darkModeButton.appendChild(sun);
   } else {
-    darkModeButton.textContent = '';
-    const moon = document.querySelector('.moon').content.cloneNode(true);
+    darkModeButton.textContent = "";
+    const moon = document.querySelector(".moon").content.cloneNode(true);
     darkModeButton.appendChild(moon);
   }
 };
 
-darkModeButton.addEventListener('click', (e) => {
-  if(verifyForMoon(e) || verifyForSun(e)){
-  const lights = document.querySelectorAll('.light');
-  lights.forEach((light) => {
-    light.classList.toggle('dark');
-  });
-  console.log(e.target.id);
-  swap(e);
-}
+darkModeButton.addEventListener("click", (e) => {
+  if (verifyForMoon(e) || verifyForSun(e)) {
+    const lights = document.querySelectorAll(".light");
+    lights.forEach((light) => {
+      light.classList.toggle("dark");
+    });
+    console.log(e.target.id);
+    swap(e);
+  }
 });
